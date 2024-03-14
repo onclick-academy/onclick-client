1c7dd167cef8d41d915ac85ffb1706056b4fbcb6
0marSalah <omar.salah1597@gmail.com>
2024-03-14 23:01:52 +0200

    fix(front): Add Firebase and Firebase Admin dependencies, update page.tsx, and remove unused SVG files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,25 +2,22 @@
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import "../styles/globals.css";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { lightBlue } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { lightBlue } from "@mui/material/colors";
 
 const darkTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
       light: lightBlue[100],
       main: lightBlue[200],
-      dark: lightBlue[400],
-    },
-  },
+      dark: lightBlue[400]
+    }
+  }
 });
 
-
 const inter = Inter({ subsets: ["latin"] });
-
-
 
 export default function RootLayout({
   children
@@ -30,16 +27,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-        <main className={styles.main}>
-          <div className={styles.center}>
-            <div className={styles.logo} />
-          </div>
-          {children}
-        </main>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <main className={styles.main}>
+            <div className={styles.center}>
+              <div className={styles.logo} />
+            </div>
+            {children}
+          </main>
         </ThemeProvider>
-        </body>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,35 @@
-"use client";
-import Link from "next/link";
-import styles from "./page.module.css";
-import getData from "@/utilities/getUserData";
-import { useEffect } from "react";
+'use client';
+import Link from 'next/link';
+import styles from './page.module.css';
+import getData from '@/utilities/getUserData';
+import { useEffect } from 'react';
+import { getDeviceToken } from '@/utilities/device';
 
 export default function Home() {
+  // const data = await getData();
+  // console.log("data", data);
+  const fetchDeviceToken = async () => {
+    const deviceToken = await getDeviceToken();
+    console.log('deviceToken', deviceToken);
+    // TODO save the token in DB
+  };
   useEffect(() => {
-    async function fetchData() {
-      const data = await getData();
-      console.log("data", data);
-    }
-  },[])
+    fetchDeviceToken();
+    const eventSource = new EventSource(
+      'http://localhost:3000/api/v1/notifications/real-time'
+    );
+    eventSource.onmessage = function (event) {
+      console.log('New message:', JSON.parse(event.data));
+    };
+    return () => {
+      eventSource.close();
+    };
+  }, []);
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <Link href={"/register"}>Register</Link>
-      </div>
-      <div className={styles.center}>
-        <div className={styles.logo} />
+        <Link href={'/register'}>Register</Link>
       </div>
     </main>
   );
