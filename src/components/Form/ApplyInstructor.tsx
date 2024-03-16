57bc8f7426bc04a1f75d53e3a3a6dbde3a29f790
Kholoud Fattem <kfattem@gmail.com>
2024-03-16 04:42:02 +0200

    fix[frontend]: Update the html tags to typography tags

--- src/components/Form/ApplyInstructor.tsx
+++ src/components/Form/ApplyInstructor.tsx
@@ -1,24 +1,26 @@
-"use client";
-import * as React from "react";
-import { useForm } from "react-hook-form";
-import { Box, Button, FormControl, FormLabel, Input } from "@mui/material";
+'use client';
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Box, Button, FormControl, Typography, TextField, useMediaQuery } from "@mui/material";
 import { useRouter } from "next/navigation";
 import "../../styles/applyInstructor.css";
 
 export const ApplyInstructor = () => {
-  const { register, handleSubmit } = useForm();
-  const [view, setView] = React.useState("view");
-
+  const { register, handleSubmit, control } = useForm();
+  const [view, setView] = useState("view");
+  const [cv, setCv] = useState("");
   const router = useRouter();
+  const mediaQuery = useMediaQuery("(max-width: 800px)");
+
   const applyInstructor = async (data: any) => {
     const user = localStorage.getItem("user");
     const userObj = JSON.parse(user);
     const userId = userObj.id;
-    data.userId = "userId";
+    data.userId = userId;
 
     console.log(data);
 
-    const url = "http://localhost:3000/api/v1/instructors/"
+    const url = "http://localhost:3000/api/v1/instructors/";
     const response = await fetch(url, {
       method: "POST",
       headers: {
@@ -39,41 +41,41 @@ export const ApplyInstructor = () => {
     <Box className="instructorApply">
       {view === "view" ? (
         <div>
-          <h1>
+          <Typography variant="h3">
             Want To Join Our Team? <br /> Spread the knowledge and help students
-            start their carier ?
-          </h1>
+            start their career?
+          </Typography>
           <hr />
-          <br />
-          <p>
+          <Typography variant="body1">
             You're most welcomed! as We welcome passionate individuals who are
             eager to share their knowledge and contribute to the growth of
             aspiring professionals.
-          </p>
-          <h2>Main Role of the Instructor: </h2>
-          <p>
+          </Typography>
+          <Typography variant="h5">Main Role of the Instructor: </Typography>
+          <Typography variant="body1">
             As an instructor, you will play a vital role in guiding and
             mentoring students, helping them to kickstart their career in the
             desired field. <br /> You will be responsible for creating and
             delivering high-quality content, providing constructive feedback,
             and supporting students in their learning journey.
-          </p>
-          <h2>- Conditions and Rules</h2>
+          </Typography>
+          <Typography variant="h5">- Conditions and Rules</Typography>
           <ul>
-            <li>Hold relevant expertise and experience in the field.</li>
-            <li>Commit to delivering high-quality instruction.</li>
-            <li>Adhere to the platform's code of conduct and policies.</li>
-            <li>Provide constructive feedback and support to students.</li>
-            <li>
-              Work with the platform to ensure a smooth learning experience.
-            </li>
+            <Typography variant="body1">
+              <li>Hold relevant expertise and experience in the field.</li>
+              <li>Commit to delivering high-quality instruction.</li>
+              <li>Adhere to the platform's code of conduct and policies.</li>
+              <li>Provide constructive feedback and support to students.</li>
+              <li>
+                Work with the platform to ensure a smooth learning experience.
+              </li>
+            </Typography>
           </ul>
-
-          <h4>Apply Now</h4>
-          <p>
+          <Typography variant="h6">Apply Now</Typography>
+          <Typography variant="body1">
             To apply as an instructor, you need to provide your CV and National
             ID. You can either upload your CV or provide a link to it.
-          </p>
+          </Typography>
           <div className="applyBtn">
             <Button
               variant="contained"
@@ -86,39 +88,75 @@ export const ApplyInstructor = () => {
         </div>
       ) : (
         <div className="applyInstForm">
-            <div className="applyFormHeading">
-        <h1>Apply Now</h1>
-        <p>We will be so glad to have you! please fill this form and wait for our response!</p>
-        </div>
-        <form onSubmit={handleSubmit(applyInstructor)}>
-          <FormControl fullWidth>
-            <Input {...register("cv")} type="text" placeholder="CV Link" />
-          </FormControl>
-
-          <FormControl fullWidth>
-            <FormLabel>or Upload Your CV</FormLabel>
-            <Input {...register("cv")} type="file" />
-          </FormControl>
-
-          <FormControl fullWidth>
-            <Input {...register("nationalId")} type="text" placeholder="National ID" />
-          </FormControl>
-
-          <div className="applybtnForm">
-            <Button type="submit" variant="contained" color="primary">
-              Submit
-            </Button>
-            <Button
-              type="button"
-              onClick={() => {
-                router.push("/");
-              }}
-              variant="contained"
-            >
-              Cancel
-            </Button>
+          <div className="applyFormHeading">
+            <Typography variant="h3">Apply Now</Typography>
+            <Typography variant="body1">
+              We will be so glad to have you! please fill this form and wait for
+              our response!
+            </Typography>
           </div>
-        </form>
+          <Box
+            component="form"
+            onSubmit={handleSubmit(applyInstructor)}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+            }}
+          >
+            <FormControl fullWidth>
+              <TextField
+                variant="outlined"
+                {...register("cvLink")}
+                type="text"
+                label="CV Link"
+              />
+            </FormControl>
+            <FormControl fullWidth>
+              <Controller
+                name="cvLink"
+                control={control}
+                render={({ field: { onChange, onBlur, name, ref } }) => (
+                  <Button
+                    variant="contained"
+                    component="label"
+                    sx={{
+                      width: mediaQuery? "8rem" : "12.5rem",
+                    }}
+                  >
+                    {mediaQuery ? "Upload CV" : "Or Upload Your CV"}
+                    <input
+                      hidden
+                      type="file"
+                      onChange={(e) => {
+                        onChange(e.target.files);
+                        setCv(e.target.files[0].name);
+                      }}
+                      onBlur={onBlur}
+                      name={name}
+                      ref={ref}
+                      accept=".pdf,.doc,.docx"
+                    />
+                  </Button>
+                )}
+              />
+              {cv}
+            </FormControl>
+            <div className="applybtnForm">
+              <Button type="submit" variant="contained" color="primary">
+                Submit
+              </Button>
+              <Button
+                type="button"
+                onClick={() => {
+                  router.push("/");
+                }}
+                variant="text"
+              >
+                Cancel
+              </Button>
+            </div>
+          </Box>
         </div>
       )}
     </Box>
